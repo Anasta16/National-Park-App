2ff7487dd1d4a5291cbefcb664b33bfdff04916e
Anasta16 <97866619+Anasta16@users.noreply.github.com>
2023-04-03 00:05:38 -0400

    json fixed

---
+++ db_json_editor.js
@@ -0,0 +1,55 @@
+// {
+//     "id": "0",
+//     "url": "https://www.nps.gov/acad/index.htm",
+//     "fullName": "Acadia National Park",
+//     "description": "Acadia National Park protects the natural beauty of the highest rocky headlands along the Atlantic coastline of the United States, an abundance of habitats, and a rich cultural heritage. At 4 million visits a year, it's one of the top 10 most-visited national parks in the United States. Visitors enjoy 27 miles of historic motor roads, 158 miles of hiking trails, and 45 miles of carriage roads.",
+//     "latitude": "44.409286",
+//     "longitude": "-68.247501",
+//     "activities": [
+//         "Stargazing",
+//         "Biking",
+//         "Boating",
+//         "Camping",
+//         "Rock Climbing",
+//         "Fishing",
+//         "Hiking",
+//         "Horseback Riding",
+//         "Canoeing",
+//         "Snowmobiling"
+//     ],
+//     "states": "ME",
+//     "directionsInfo": "From Boston take I-95 north to Augusta, Maine, then Route 3 east to Ellsworth, and on to Mount Desert Island. For an alternate route, continue on I-95 north to Bangor, Maine, then take I-395 to U.S. Route 1A east to Ellsworth. In Ellsworth, take Route 3 to Mount Desert Island.",
+//     "directionsUrl": "http://www.nps.gov/acad/planyourvisit/directions.htm",
+//     "image": "https://www.nps.gov/common/uploads/structured_data/3C7B45AE-1DD8-B71B-0B7EE131C7DFC2F5.jpg",
+//     "weatherInfo": "Located on Mount Desert Island in Maine, Acadia experiences all four seasons. Summer temperatures range from 45-90F (7-30C). Fall temperatures range from 30-70F (-1-21C). Typically the first frost is in mid-October and first snowfall begins in November and can continue through April with an average accumulation of 73 inches (185 cm). Winter temperatures range from 14-35F (-10 - 2C). Spring temperatures range from 30-70F (-1-21C)."
+//   },
+
+
+// 20230402210332
+// http://localhost:3000/national-park-listings/3
+
+{
+    "id": "3",
+    "url": "https://www.nps.gov/bibe/index.htm",
+    "fullName": "Big Bend National Park",
+    "description": "There is a place in Far West Texas where night skies are dark as coal and rivers carve temple-like canyons in ancient limestone. Here, at the end of the road, hundreds of bird species take refuge in a solitary mountain range surrounded by weather-beaten desert. Tenacious cactus bloom in sublime southwestern sun, and diversity of species is the best in the country. This magical place is Big Bend...",
+    "latitude": "29.29817767",
+    "longitude": "-103.2297897",
+    "activities": [
+        "Scenic Driving",
+        "Stargazing",
+        "Biking",
+        "Camping",
+        "Fishing",
+        "Hiking",
+        "Paddling",
+        "Kayaking",
+        "Birdwatching"
+    ],
+    "states": "TX",
+    "directionsInfo": "Several highways lead to Big Bend National Park: TX 118 from Alpine to Study Butte or FM 170 from Presidio to Study Butte (then 26 miles east to park headquarters) or US 90 or US 385 to Marathon (then 70 miles south to park headquarters). Distances between towns and services are considerable. Be sure you have plenty of gas, food, water, and supplies for your trip. There are no electric vehicle charging stations in Big Bend National Park. The closest EV charging station is 130 miles away in Fort Stockton.",
+    "directionsUrl": "http://www.nps.gov/bibe/planyourvisit/directions.htm",
+    "image": "https://www.nps.gov/common/uploads/structured_data/8BF8356B-BB63-76A4-19F5296EF94C96B4.jpg",
+    "weatherInfo": "Big Bend is a desert park, so be prepared for extreme conditions. SPRING: From February through April the park abounds with pleasant and comfortable temperatures. SUMMER: May through September is very hot. Temperatures will exceed 100 degrees in the lower elevations by late morning, and often exceed 110 along the Rio Grande. Afternoon thunderstorms can be a welcome respite. WINTER: October through January temperatures are cooler; the weather can quickly turn cold at any time during these months."
+  },
+
